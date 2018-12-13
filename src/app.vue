da3ade7fe7f77524de736c45ff78e572920fa065
Feroz Ahmmed <froghramar@gmail.com>
2018-12-13 16:32:56 +0600

    define grid layout

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="grid-container" id="app">
+        <div class="ocf-header">Online Code Formatter</div>
+        <div class="ocf-left-nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+        <div class="ocf-main-content">
+            <router-view/>
+        </div>
+        <div class="ocf-footer">Footer</div>
+    </div>
+</template>
+
+<style lang="scss" src="./app.scss"></style>

b0d9b43db28cd79bcf8c1c4aae13bb2ac4eaf021
Feroz Ahmmed <froghramar@gmail.com>
2018-12-13 17:07:28 +0600

    use vuetify

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,24 @@
 <template>
-    <div class="grid-container" id="app">
-        <div class="ocf-header">Online Code Formatter</div>
-        <div class="ocf-left-nav">
-            <router-link to="/">Home</router-link> |
+    <v-app>
+        <v-navigation-drawer app>
+            <router-link to="/">Home</router-link>
             <router-link to="/about">About</router-link>
-        </div>
-        <div class="ocf-main-content">
+        </v-navigation-drawer>
+        <v-content>
             <router-view/>
-        </div>
-        <div class="ocf-footer">Footer</div>
-    </div>
+        </v-content>
+    </v-app>
 </template>
 
 <style lang="scss" src="./app.scss"></style>
+
+<script lang="ts">
+
+    export default {
+        name: 'App',
+        components: { },
+        data: () => {
+            return { };
+        },
+    };
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import './plugins/vuetify';
 import App from './app.vue';
 import router from './router';
 
